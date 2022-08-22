3fac4ed8c2740cfd7c98b11f84fc39125312c8f7
rey-hossa <rey.hossa.rh@gmail.com>
2022-08-22 07:18:40 +0200

    add production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: true,
-  apiUrl: 'https://progettofinale-backend.herokuapp.com'
-  // apiUrl: 'http://127.0.0.1:8000'
-};
+// export const environment = {
+//   production: true,
+//   apiUrl: 'https://progettofinale-backend.herokuapp.com'
+//   // apiUrl: 'http://127.0.0.1:8000'
+// };
 
 // export const environment = {
 //   production: true,
